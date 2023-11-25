71de8e8e6526db7bdc532e12dc4fb911ffc1f855
andyim2004 <andyim2004@gmail.com>
2023-11-25 15:39:56 +0900

    'lasertimeadd'

--- Assets/Scripts/LineRenderer/LineWidthControl.cs
+++ Assets/Scripts/LineRenderer/LineWidthControl.cs
@@ -7,12 +7,13 @@ public class LineWidthControl : MonoBehaviour
     // Start is called before the first frame update
     public LineWidthChange1 script1;
     public LineWidthChange2 script2;
-
+    [SerializeField] private float time1 = 1.5f;
+    [SerializeField] private float time2 = 2.0f;
     void Start()
     {
-        Invoke("StartScript1", 1.5f);
+        Invoke("StartScript1", time1);
 
-        Invoke("StartScript2", 2.0f);
+        Invoke("StartScript2", time2);
 
     }
 
